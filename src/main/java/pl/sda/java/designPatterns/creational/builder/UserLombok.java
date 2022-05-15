f9f6e5c2ffe763d6c1d325d5a8924926d1f3695f
Nela Kuklake <aniela.kacka@gmail.com>
2022-05-15 11:39:20 +0200

    add lombok to pom

---
+++ src/main/java/pl/sda/java/designPatterns/creational/builder/UserLombok.java
@@ -0,0 +1,9 @@
+package pl.sda.java.designPatterns.creational.builder;
+
+public class UserLombok {
+    private String firstName; // required
+    private String lastName; // required
+    private String phone;
+    private String address;
+    private int age;
+}
---
+++ src/test/java/pl/sda/java/designPatterns/creational/builder/UserLombokTest.java
@@ -0,0 +1,12 @@
+package pl.sda.java.designPatterns.creational.builder;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserLombokTest {
+
+    @Test
+    void name() {
+    }
+}
